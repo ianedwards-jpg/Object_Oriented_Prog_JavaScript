e4dbcefb6b09b61ec08668f4f2e39afd37cf039a
Ian Edwards <ian.edward@uconn.edu>
2021-06-09 12:03:41 -0400

    Update animalAgeSolution.js
    Remove unneeded comma at the end of animalFax array
    More clarification of console outputs for user readability
    Added semicolons where needed
    Adjusted spaces in several spots for readability

--- animalAgeSolution.js
+++ animalAgeSolution.js
@@ -14,30 +14,30 @@ const animalFax = [
     { Name: "Lion",  Age: 31 },
     { Name: "Leopard",  Age: 67 },
     { Name: "Ostritch",  Age: 12 },
-    { Name: "Hyena",  Age: 54 },
-] // defines the animals name and age as objects within an array 
+    { Name: "Hyena",  Age: 54 }
+]; // defines the animals name and age as objects within an array 
 
 function compute() {
 
 let targetAge = 29; 
 let youngestOld = ''; //Empty Array to be filled
-let oldAnimals = new Array();// In a production setting, you could also use oldAnimals = [] 
+let oldAnimals = new Array(); // In a production setting, you could also use oldAnimals = [] 
 
     for (let i = 0; i < animalFax.length; i++) {
 
         if (animalFax[i].Age > targetAge)  {
         oldAnimals.push({Name: animalFax[i].Name, Age: animalFax[i].Age})
-        }// If the animals Age is over 29, their name and age are added to the oldAnimals array as an object 
+        }; // If the animals Age is over 29, their name and age are added to the oldAnimals array as an object 
 
         // console.log(animalFax[i].Name) Returns all Names in animalFax array 
         // console.log(animalFax[i].Age)  Returns all ages in animalFax array 
-     }
+    };
      
-    oldAnimals.sort((a, b) => (a.Age > b.Age) ? 1 : -1) // Sorts the animal objects in the oldAnimals array by Age
+    oldAnimals.sort((a, b) => (a.Age > b.Age) ? 1 : -1); // Sorts the animal objects in the oldAnimals array by Age
     youngestOld = oldAnimals[0].Name; // Assigns the Name of the first member of the sorted oldAnimals array to the youngestOld variable
     
-    //  console.log(oldAnimals[0].Name)
-     console.log(youngestOld) // Outputs Lion to console.
+    //  console.log(oldAnimals[0].Name) // Expected output: Lion 
+    console.log(youngestOld); // Outputs youngestOld value to console. Expected output: Lion
 };
 
 compute();
