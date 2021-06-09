4afef1ddb9256dcbe9cade95b5810e168cda60d7
Ian Edwards <ian.edward@uconn.edu>
2021-06-08 22:21:41 -0400

    First Commit on Repo
    Added first project

---
+++ animalAge4.js
@@ -0,0 +1,42 @@
+var animalFax = [
+    { Name: "Giraffe", Age: 9 },
+    { Name: "Piranha",  Age: 27 },
+    { Name: "Gazelle",  Age: 31 },
+    { Name: "Lion",  Age: 45 },
+    { Name: "Leopard",  Age: 67 },
+    { Name: "Ostritch",  Age: 12 },
+]
+
+function compute() {
+
+var oldCeiling = 40;
+var youngCeiling = 27;
+var targetAge = 29; 
+var name = ''
+
+    for (let i = 0; i < animalFax.length; i++) {
+          console.log(animalFax[i].Name)
+        }
+
+        // if (animalFax[i].Age < oldCeiling) animalFax.Name[i] = name;
+
+    // if (animalFax[4].Age > oldCeiling)  {
+    //     name = animalFax[4].Name;
+    // }
+   
+    
+    
+}
+
+compute();
+
+//AnimalFax.Length == Good || console.log(JSON.stringify(animalFax.length))
+//console.log(JSON.stringify(animalFax[1].Name)) ALWAYS PUT THE .Identifier AFTER THE SQUARE BRACKETS 
+
+ // console.log(JSON.stringify(age)) // Returns String 
+    // console.log(JSON.stringify(animalFax[1].Name))
+    // console.log(JSON.stringify(animalFax[1].Age))
+    // console.log(animalFax[1].Name)
+    // console.log(animalFax[1].Age)
+    // console.log(JSON.stringify(name))
+    // console.log(name)
